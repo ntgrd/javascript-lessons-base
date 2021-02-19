ca1081d28cc63b2c1672e35ae31d8ece431ae06e
Natalia Kozlova <natalya.v.gordeeva@gmail.com>
2021-02-19 15:28:16 +0500

    Начало работы над первым уроком.

---
+++ lesson1.js
@@ -0,0 +1,14 @@
+'use strict'
+
+let tC = 32;
+let tF= (9 / 5) * tC + 32
+    console.log(tF);
+
+let admin
+let name= 'Василий'
+    admin=name
+    console.log(admin)
+
+let a = 1000
+let b = '108'
+    console.log(a+b)
