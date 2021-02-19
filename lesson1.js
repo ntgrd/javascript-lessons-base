3e114ecebeb361e977fbf6e0e1c4694a50496219
Natalia Kozlova <natalya.v.gordeeva@gmail.com>
2021-02-19 15:39:27 +0500

    исправлено форматирование файла

--- lesson1.js
+++ lesson1.js
@@ -1,14 +1,16 @@
 'use strict'
 
-let tC = 32;
-let tF= (9 / 5) * tC + 32
-    console.log(tF);
+const tC = 32;
+let tF = (9 / 5) * tC + 32;
+console.log(tF);
 
-let admin
-let name= 'Василий'
-    admin=name
-    console.log(admin)
+let admin;
+const name = 'Василий';
+admin = name;
+console.log(admin);
 
-let a = 1000
-let b = '108'
-    console.log(a+b)
+let a = 1000;
+let b = '108';
+console.log(a + b);
+
+
